fdc249d1b825fd7e371a13237fe524aa93b4e525
David de Llano Garcia <dllano@conalep.edu.mx>
2023-12-19 11:16:29 -0600

    prueba de actualizacion

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -26,7 +26,9 @@
                             </div>
                             <header class="bg-white" v-if="$slots.header">
                                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                                    Menú
+                                    <NavLink>
+                                        
+                                    </NavLink>
                                 </div>
                             </header>
                         </div>
