bee79d04d862487b640556480aeeb2c3f10186a1
David de Llano Garcia <dllano@conalep.edu.mx>
2023-12-19 11:18:37 -0600

    Inicio correcto

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -26,9 +26,7 @@
                             </div>
                             <header class="bg-white" v-if="$slots.header">
                                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                                    <NavLink>
-                                        
-                                    </NavLink>
+                                    
                                 </div>
                             </header>
                         </div>
